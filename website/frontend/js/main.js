534c8ec16eb576ff288fdd061fe46bd267d4cb57
Tyson Nguyen <AKATYSON_XD@hotmail.com>
2025-01-22 10:45:49 +1100

    updated main.js uncommented api fetch

--- website/frontend/js/main.js
+++ website/frontend/js/main.js
@@ -7,10 +7,9 @@ console.log("JS triggered")
 // #Function sends a request to lambda function which lambda will return JSON file containing the updated view counter data.
 async function updateCounter() {
   try{
-    // let response = await fetch("https://kn3ighjdokzlf7gzssmkvzom2y0kcubx.lambda-url.ap-southeast-2.on.aws/")
-    // let data = await response.json();
-    let data = 25;
-    // console.log(`aws lambda respond with current view: ${data}`);
+    let response = await fetch("https://kn3ighjdokzlf7gzssmkvzom2y0kcubx.lambda-url.ap-southeast-2.on.aws/")
+    let data = await response.json();
+    console.log(`aws lambda respond with current view: ${data}`);
     document.getElementById("counter").innerText = data;
     console.log("exit function")
   } catch(error){
