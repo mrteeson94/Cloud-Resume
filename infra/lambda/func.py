3686c71e5c31aa04bf4a32d57147d88c2658af0c
Tyson Nguyen <AKATYSON_XD@hotmail.com>
2025-01-21 14:21:37 +1100

    added terraform folder for version control

---
+++ infra/lambda/func.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('aws-counter')
+
+
+def lambda_handler(event, context):
+    response = table.get_item(Key={'id': '1'})
+    views = response['Item']['views']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={'id': '1', 'views': views})
+    return views
